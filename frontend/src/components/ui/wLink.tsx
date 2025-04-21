8833bf48eaca08ceeab12e1130529cb7c065b899
gkampitakis <gkabitakis@gmail.com>
2025-04-21 11:33:42 +0300

    fix: add support for signing with cosign

--- frontend/src/components/ui/wLink.tsx
+++ frontend/src/components/ui/wLink.tsx
@@ -3,12 +3,17 @@ import { BrowserOpenURL } from "wailsjs/runtime/runtime";
 
 interface Props {
   href: string;
+  className?: string;
 }
 
-const WLink: React.FC<PropsWithChildren<Props>> = ({ href, children }) => {
+const WLink: React.FC<PropsWithChildren<Props>> = ({
+  href,
+  children,
+  className,
+}) => {
   return (
     <span
-      className="cursor-pointer text-xs underline"
+      className={`cursor-pointer text-xs underline ${className}`}
       onClick={() => BrowserOpenURL(href)}
     >
       {children}
