858fbb1629077a28904af2055d33987935d5e0d5
gkampitakis <gkabitakis@gmail.com>
2026-03-14 21:23:52 +0200

    remove newtab component

--- frontend/src/components/tabs/util.ts
+++ frontend/src/components/tabs/util.ts
@@ -1,7 +1,6 @@
 import { Tab } from "@/types";
 import { WelcomeName } from "./Welcome";
-import { NewTabName } from "./NewTab";
 import { ClusterDetailsName } from "./ClusterDetails/ClusterDetails";
 
 export const isGeneralTab = (tab: Tab): boolean =>
-  [WelcomeName, NewTabName, ClusterDetailsName].includes(tab.name);
+  [WelcomeName, ClusterDetailsName].includes(tab.name);
